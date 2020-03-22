da94fc8c35dbfa9b04b71c8e0175e697511d9b17
Rafael Costa <rafaelcostasaboia@yahoo.com.br>
2020-03-22 18:51:43 -0300

    Recurso Ordenação

--- odenacao.php
+++ odenacao.php
@@ -1,14 +1,13 @@
 <?php
 
+    //Valores a serem ordenados
     $array = [31, 41, 59, 26, 41, 58];
-    
-    $ordenacao = [];
-    $aux = 0;
     
     for($i = 0; $i < count($array); $i++)
     {   
         for($j = $i; $j < count($array); $j++)
         {
+            //O sinal determina o tipo de ordenação ('>' - Ordenação crescente || '<' - Ordenação decrescente) 
             if($array[$i] > $array[$j])
             {
                 $aux = $array[$j];
